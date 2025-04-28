7cccd8f61d787c0f1c980ba1ed0c4e791ed2dad3
Bossola-Yannick <bossolay@hotmail.fr>
2025-04-28 10:48:28 +0200

    feat: upgrade data.sql and bdd.excalidraw schema

--- data.sql
+++ data.sql
@@ -1,15 +1,14 @@
 -- EFFACEMENT DES TABLES
+DROP TABLE IF EXISTS product_tag;
 DROP TABLE IF EXISTS product_cart;
 DROP TABLE IF EXISTS product_orders;
-DROP TABLE IF EXISTS product_subcategory;
-DROP TABLE IF EXISTS product_category;
 DROP TABLE IF EXISTS comment;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS sub_category;
-DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS tag;
 
 -- Création de la table USER
 CREATE TABLE user (
@@ -33,26 +32,23 @@ CREATE TABLE product (
     price_ht DECIMAL(10,2) NOT NULL,
     price_ttc DECIMAL(10,2) NOT NULL,
     price_discount DECIMAL(10,2) NOT NULL,
-    image_link VARCHAR(100)
-) ENGINE=InnoDB;
-
--- Création de la table catégorie
-CREATE TABLE category (
-    id_category INT AUTO_INCREMENT PRIMARY KEY,
-    name_category VARCHAR(100) NOT NULL
+    image_link VARCHAR(100),
+    category VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
 -- Création de la table sous-catégorie
 CREATE TABLE sub_category (
     id_subcategory INT AUTO_INCREMENT PRIMARY KEY,
-    name_subcategory VARCHAR(100) NOT NULL
+    name_subcategory VARCHAR(100) NOT NULL,
+    id_product INT,
+    FOREIGN KEY (id_product) REFERENCES product(id_product)
 ) ENGINE=InnoDB;
 
 -- Création de la table commande
 CREATE TABLE orders (
     id_order INT AUTO_INCREMENT PRIMARY KEY,
     num_order INT NOT NULL UNIQUE,
-    date_order DATE,
+    date_order DATE NOT NULL,
     id_user INT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES user(id_user)
 ) ENGINE=InnoDB;
@@ -62,38 +58,35 @@ CREATE TABLE cart(
     id_cart INT AUTO_INCREMENT PRIMARY KEY,
     id_user INT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES user(id_user) ON DELETE CASCADE
-)
+) ENGINE=InnoDB;
 
 -- Création de la table commentaire
 CREATE TABLE comment (
     id_comment INT AUTO_INCREMENT PRIMARY KEY,
     rating INT,
-    comment VARCHAR(2000) NOT NULL,
-    admin_reply VARCHAR(2000),
+    comment TEXT NOT NULL,
+    date_comment DATE NOT NULL,
+    admin_reply TEXT,
     id_product INT,
     id_user INT,
     FOREIGN KEY (id_product) REFERENCES product(id_product) ON DELETE CASCADE,
     FOREIGN KEY (id_user) REFERENCES user(id_user) 
 ) ENGINE=InnoDB;
 
+CREATE TABLE tag (
+    id_tag INT AUTO_INCREMENT PRIMARY KEY,
+    name_tag VARCHAR(100) NOT NULL
+) ENGINE=InnoDB;
 
 -- TABLE DE LIAISON --
 
 -- Création de la table produit - catégorie
-CREATE TABLE product_category (
+CREATE TABLE product_tag (
     id_product INT,
-    id_category INT, 
-    PRIMARY KEY (id_product, id_category),
+    id_tag INT, 
+    PRIMARY KEY (id_product, id_tag),
     FOREIGN KEY (id_product) REFERENCES product(id_product),
-    FOREIGN KEY (id_category) REFERENCES category(id_category)
-) ENGINE=InnoDB;
--- Création de la table produit - sous-catégorie
-CREATE TABLE product_subcategory (
-    id_product INT,
-    id_subcategory INT, 
-    PRIMARY KEY (id_product, id_subcategory),
-    FOREIGN KEY (id_product) REFERENCES product(id_product),
-    FOREIGN KEY (id_subcategory) REFERENCES sub_category(id_subcategory)
+    FOREIGN KEY (id_tag) REFERENCES tag(id_tag)
 ) ENGINE=InnoDB;
 -- Création de la table produit - commande
 CREATE TABLE product_orders (
