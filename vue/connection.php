a98149631b61ac46d2d7df9410539632149a0caa
Bossola-Yannick <bossolay@hotmail.fr>
2025-04-29 14:30:27 +0200

    feat: added connection and registration vue

--- vue/connection.php
+++ vue/connection.php
@@ -2,17 +2,15 @@
 include '../components/header.php';
 ?>
 <section class="connection">
-    <div class="connection-box">
-        <form action="" id="form-connection">
-            <label for="email">Votre Email</label>
-            <input type="email" name="email" id="email" />
-            <span id="emailError"></span>
-            <label for="password">Votre Mot de Passe</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit" class="connection-button">Connexion</button>
-            <span id="connection-message"></span>
-        </form>
-    </div>
+    <form action="" id="form-connection">
+        <label for="email">Votre Email</label>
+        <input type="email" name="email" id="email" />
+        <span id="emailError"></span>
+        <label for="password">Votre Mot de Passe</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit" class="connection-button button">Connexion</button>
+        <span id="connection-message"></span>
+    </form>
 </section>
 <?php
 include '../components/footer.php';
--- vue/registration.php
+++ vue/registration.php
@@ -1,30 +1,31 @@
 <?php
 include '../components/header.php';
 ?>
-<main>
+<section class="registration">
     <form action="" id="form-registration">
         <h2 id="status-registration"></h2>
-        <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" />
-        <label for="prenom">Prénom</label>
-        <input type="text" name="prenom" id="prenom" />
         <label for="email">Email</label>
         <input type="email" name="mail" id="email" />
         <span id="emailError"></span>
-        <label for="adresse">Adresse</label>
-        <input type="text" name="adresse" id="adresse" />
-        <label for="codeP">Code Postale</label>
-        <input type="number" name="codeP" id="codeP" />
-        <span id="codePostalError"></span>
         <label for="password">Mot de Passe</label>
         <input type="password" name="password" id="password" />
         <span id="passwordError"></span>
         <label for="confirmPassword">Confirmtion Mot de Passe</label>
         <input type="password" name="password" id="confirmPassword" />
         <span id="identiquePasswordError"></span>
-        <button type="submit">Valider</button>
+        <h2 class="subtitle-registration">Mes Informations</h2>
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom" />
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" id="prenom" />
+        <label for="adresse">Adresse</label>
+        <input type="text" name="adresse" id="adresse" />
+        <label for="codeP">Code Postale</label>
+        <input type="number" name="codeP" id="codeP" />
+        <span id="codePostalError"></span>
+        <button type="submit" class="button">Valider</button>
     </form>
-</main>
+</section>
 <?php
 include '../components/footer.php';
 ?>
