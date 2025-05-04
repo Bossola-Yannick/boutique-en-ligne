904f694d692569ac0711a29a6c15e71786891aeb
James Sanchez <james.sanchez@laplateforme.io>
2025-05-04 19:54:16 +0200

    add: cart number item element in header

--- components/header.php
+++ components/header.php
@@ -51,6 +51,7 @@ session_start();
         <div class="cart-box">
             <a href="">
                 <img src="../assets/images/icones/cart.png" alt="">
+                <p class="cart-number-items">0</p>
             </a>
         </div>
     </header>
