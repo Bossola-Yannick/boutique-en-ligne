a8ce65d7506320ed5bc190a39e6d5ad67776b889
James SANCHEZ <james.sanchez@laplateforme.io>
2025-05-10 16:00:52 +0200

    feat: include banner component in accessories, costumes, and promo pages

--- vue/accessories.php
+++ vue/accessories.php
@@ -2,10 +2,7 @@
 
 include '../components/header.php';
 include '../components/search.php';
-
-$_SESSION['user_id'] = 2;
-$_SESSION['user_name'] = "james";
-$_SESSION['user_role'] = "user";
+include '../components/banner.php';
 
 ?>
 
--- vue/costumes.php
+++ vue/costumes.php
@@ -2,6 +2,7 @@
 
 include '../components/header.php';
 include '../components/search.php';
+include '../components/banner.php';
 
 $_SESSION['user_id'] = 2;
 $_SESSION['user_name'] = "james";
